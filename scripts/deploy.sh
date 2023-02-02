9727276d721944084606fc8bf04f427fe0ba527d
shizhu <shizhu@yaomaitong.cn>
2023-02-02 17:37:06 +0800

    修改actions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,7 +22,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/trry-github/docs.git main
+git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main:gh-pages
 
 cd -
 
