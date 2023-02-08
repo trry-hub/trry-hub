a7bbee536a2db9cc4813c9d15445b004402064fe
shizhu <shizhu@yaomaitong.cn>
2023-02-08 18:18:52 +0800

    更新文档

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,10 +5,11 @@ import sidebar from "../../utils/sidebar";
 
 export default defineConfig({
     title: "trry-blog",
-    base: '/docs/',
+    base: "/trry/",
     lang: 'zh-CN',
     description: "简单记录生活",
     head: [
+        ["link", { rel: 'icon', href: './favicon.ico' }],
         [
             "link",
             {
@@ -19,6 +20,10 @@ export default defineConfig({
     ],
     lastUpdated: true,
     themeConfig: {
+        logo: '/trry.png',
+        footer: {
+            copyright: 'Copyright © 2020-present trry'
+        },
         editLinkText: "在 GitHub 上编辑此页",
         lastUpdatedText: "上次更新",
         docFooter: {
@@ -34,52 +39,48 @@ export default defineConfig({
             pattern: 'https://github1s.com/trry-github/-Learning/edit/docs/docs/:path',
             text: '在GitHub上编辑此页面'
         },
+        outline: 'deep',
         algolia: {
             appId: 'M2GO13PB0J',
             apiKey: "dea55947cd635962d4413464ef0e4595",
             icon: "/public/trry-github.svg",
             indexName: "docs",
-            locales: {
-                zh: {
-                    placeholder: '搜索文档',
-                    buttonText: '搜索',
-                    translations: {
-                        button: {
-                            buttonText: '搜索文档',
-                            buttonAriaLabel: '搜索文档'
-                        },
-                        modal: {
-                            searchBox: {
-                                resetButtonTitle: '清除查询条件',
-                                resetButtonAriaLabel: '清除查询条件',
-                                cancelButtonText: '取消',
-                                cancelButtonAriaLabel: '取消'
-                            },
-                            startScreen: {
-                                recentSearchesTitle: '搜索历史',
-                                noRecentSearchesText: '没有搜索历史',
-                                saveRecentSearchButtonTitle: '保存至搜索历史',
-                                removeRecentSearchButtonTitle: '从搜索历史中移除',
-                                favoriteSearchesTitle: '收藏',
-                                removeFavoriteSearchButtonTitle: '从收藏中移除'
-                            },
-                            errorScreen: {
-                                titleText: '无法获取结果',
-                                helpText: '你可能需要检查你的网络连接'
-                            },
-                            footer: {
-                                selectText: '选择',
-                                navigateText: '切换',
-                                closeText: '关闭',
-                                searchByText: '搜索提供者'
-                            },
-                            noResultsScreen: {
-                                noResultsText: '无法找到相关结果',
-                                suggestedQueryText: '你可以尝试查询',
-                                reportMissingResultsText: '你认为该查询应该有结果？',
-                                reportMissingResultsLinkText: '点击反馈'
-                            }
-                        }
+            placeholder: '搜索文档',
+            translations: {
+                button: {
+                    buttonText: '搜索文档',
+                    buttonAriaLabel: '搜索文档'
+                },
+                modal: {
+                    searchBox: {
+                        resetButtonTitle: '清除查询条件',
+                        resetButtonAriaLabel: '清除查询条件',
+                        cancelButtonText: '取消',
+                        cancelButtonAriaLabel: '取消'
+                    },
+                    startScreen: {
+                        recentSearchesTitle: '搜索历史',
+                        noRecentSearchesText: '没有搜索历史',
+                        saveRecentSearchButtonTitle: '保存至搜索历史',
+                        removeRecentSearchButtonTitle: '从搜索历史中移除',
+                        favoriteSearchesTitle: '收藏',
+                        removeFavoriteSearchButtonTitle: '从收藏中移除'
+                    },
+                    errorScreen: {
+                        titleText: '无法获取结果',
+                        helpText: '你可能需要检查你的网络连接'
+                    },
+                    footer: {
+                        selectText: '选择',
+                        navigateText: '切换',
+                        closeText: '关闭',
+                        searchByText: '搜索提供者'
+                    },
+                    noResultsScreen: {
+                        noResultsText: '无法找到相关结果',
+                        suggestedQueryText: '你可以尝试查询',
+                        reportMissingResultsText: '你认为该查询应该有结果？',
+                        reportMissingResultsLinkText: '点击反馈'
                     }
                 }
             }
