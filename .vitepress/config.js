26e4d5e8c5861fb6e768ff6a6fd740e8ccd918c3
shizhu <shizhu@yaomaitong.cn>
2023-03-01 10:25:31 +0800

    更新插件

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -4,7 +4,7 @@ import sidebar from "../utils/sidebar";
 // import createVitePlugins from '../vite/plugins'
 
 export default defineConfig({
-    title: "trry-blog",
+    title: "trryblog",
     lang: 'zh-CN',
     base: "/docs/",
     description: "简单记录生活",
@@ -32,11 +32,11 @@ export default defineConfig({
         },
 
         socialLinks: [
-            { icon: 'github', link: 'https://github.com/trry-github' }
+            { icon: 'github', link: 'https://github.com/trrygithub' }
         ],
 
         editLink: {
-            pattern: 'https://github.com/trry-github/-Learning/edit/docs/docs/:path',
+            pattern: 'https://github.com/trrygithub/-Learning/edit/docs/docs/:path',
             text: '在GitHub上编辑此页面'
         },
         outline: 'deep',
